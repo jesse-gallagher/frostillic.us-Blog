98e79653874d5e9e7c614354259fa757eba2b1f1
Jesse Gallagher <jesse@secondfoundation.org>
2018-07-17 10:45:51 -0400

    Filter links by visibility and lambda-ify the comparator

--- frostillicus-blog/frostillicus-blog-j2ee/src/main/java/bean/LinksBean.java
+++ frostillicus-blog/frostillicus-blog-j2ee/src/main/java/bean/LinksBean.java
@@ -18,27 +18,20 @@ public class LinksBean {
 	@Inject
 	LinkRepository links;
 	
+	private static Comparator<Link> linkComparator = (o1, o2) -> String.valueOf(o1.getName()).compareTo(o2.getName());
+	
 	public Map<String, Collection<Link>> getByCategory() {
 		Map<String, Collection<Link>> result = new TreeMap<>(String.CASE_INSENSITIVE_ORDER);
 		
 		// TODO seems like this could be done with a map collector or a Darwino query
 		links.findAll()
 			.filter(l -> l.getName() != null && !l.getName().isEmpty())
+			.filter(Link::isVisible)
 			.forEach(link -> {
-				Collection<Link> l = result.computeIfAbsent(link.getCategory(), cat -> new TreeSet<Link>(LinkComparator.INSTANCE));
+				Collection<Link> l = result.computeIfAbsent(link.getCategory(), cat -> new TreeSet<Link>(linkComparator));
 				l.add(link);
 			});
 		
 		return result;
-	}
-	
-	private static class LinkComparator implements Comparator<Link> {
-		private static LinkComparator INSTANCE = new LinkComparator();
-
-		@Override
-		public int compare(Link o1, Link o2) {
-			return String.valueOf(o1.getName()).compareTo(o2.getName());
-		}
-		
 	}
 }
