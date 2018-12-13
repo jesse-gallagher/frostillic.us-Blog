c4c1c6d038b7cde3530306f9faf2b2156c187d18
Jesse Gallagher <jesse@secondfoundation.org>
2018-12-13 16:45:44 -0500

    De-dupe some code

--- frostillicus-blog/frostillicus-blog-j2ee/src/main/java/model/PostUtil.java
+++ frostillicus-blog/frostillicus-blog-j2ee/src/main/java/model/PostUtil.java
@@ -7,6 +7,8 @@ import com.darwino.jsonstore.Store;
 import frostillicus.blog.app.AppDatabaseDef;
 
 import javax.enterprise.inject.spi.CDI;
+import java.util.Collection;
+import java.util.TreeSet;
 
 public enum PostUtil {
     ;
@@ -18,5 +20,23 @@ public enum PostUtil {
         return store.openCursor()
                 .query(JsonObject.of("form", Post.class.getSimpleName())) //$NON-NLS-1$
                 .count();
+    }
+
+    public static Collection<String> getPostMonths() throws JsonException {
+        Collection<String> months = new TreeSet<>();
+
+        Database database = CDI.current().select(Database.class).get();
+        Store store = database.getStore(AppDatabaseDef.STORE_POSTS);
+        store.openCursor()
+                .query(JsonObject.of("form", Post.class.getSimpleName())) //$NON-NLS-1$
+                .findDocuments(doc -> {
+                    String posted = doc.getString("posted"); //$NON-NLS-1$
+                    if(posted != null && posted.length() >= 7) {
+                        months.add(posted.substring(0, 7));
+                    }
+                    return true;
+                });
+
+        return months;
     }
 }
