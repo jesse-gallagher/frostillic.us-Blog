ea9b5a7b97abcb661ea62d1080fdade5e16fbee6
Jesse Gallagher <jesse@secondfoundation.org>
2019-07-30 19:03:50 -0400

    Improve RSS output

--- frostillicus-blog/frostillicus-blog-j2ee/src/main/java/api/rss20/FeedResource.java
+++ frostillicus-blog/frostillicus-blog-j2ee/src/main/java/api/rss20/FeedResource.java
@@ -22,22 +22,31 @@ import java.util.stream.Collectors;
 
 import javax.inject.Inject;
 import javax.inject.Named;
+import javax.naming.ldap.LdapName;
 import javax.servlet.ServletContext;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.UriInfo;
 
+import org.w3c.dom.Document;
+
+import com.darwino.commons.util.PathUtil;
+import com.darwino.commons.xml.DomUtil;
 import com.rometools.rome.feed.synd.SyndContent;
 import com.rometools.rome.feed.synd.SyndContentImpl;
 import com.rometools.rome.feed.synd.SyndEntry;
 import com.rometools.rome.feed.synd.SyndEntryImpl;
 import com.rometools.rome.feed.synd.SyndFeed;
 import com.rometools.rome.feed.synd.SyndFeedImpl;
+import com.rometools.rome.feed.synd.SyndImage;
+import com.rometools.rome.feed.synd.SyndImageImpl;
 import com.rometools.rome.io.FeedException;
 import com.rometools.rome.io.SyndFeedOutput;
 
+import lombok.SneakyThrows;
 import model.Post;
 import model.PostRepository;
 
@@ -49,6 +58,8 @@ public class FeedResource {
 	ResourceBundle translation;
 	@Context
 	ServletContext servletContext;
+	@Context
+	UriInfo uriInfo;
 	
 	@GET
 	@Produces("application/rss+xml")
@@ -57,23 +68,48 @@ public class FeedResource {
 		feed.setFeedType("rss_2.0"); //$NON-NLS-1$
 		feed.setTitle(translation.getString("appTitle")); //$NON-NLS-1$
 		feed.setDescription(translation.getString("appDescription")); //$NON-NLS-1$
-		feed.setLink(translation.getString("baseUrl")); //$NON-NLS-1$
+//		feed.setLink(translation.getString("baseUrl")); //$NON-NLS-1$
+		feed.setLink(uriInfo.getBaseUri().toString());
+		
+		SyndImage icon = new SyndImageImpl();
+		icon.setUrl(PathUtil.concat(uriInfo.getBaseUri().toString(), "img/icon.png", '/')); //$NON-NLS-1$
+		icon.setTitle(translation.getString("appTitle")); //$NON-NLS-1$
+		icon.setLink(uriInfo.getBaseUri().toString());
+		feed.setIcon(icon);
+		feed.setImage(icon);
 		
 		feed.setEntries(posts.homeList().stream()
 			.map(this::toEntry)
 			.collect(Collectors.toList()));
 		
-		return new SyndFeedOutput().outputString(feed);
+		Document result = new SyndFeedOutput().outputW3CDom(feed);
+		result.getDocumentElement().setAttribute("xml:base", uriInfo.getBaseUri().toString()); //$NON-NLS-1$
+		return DomUtil.getXMLString(result, false, true);
 	}
 	
+	@SneakyThrows
 	private SyndEntry toEntry(Post post) {
 		SyndEntry entry = new SyndEntryImpl();
-		entry.setAuthor(post.getPostedBy());
+		
+		String author = post.getPostedBy();
+		if(author != null && author.startsWith("cn=")) { //$NON-NLS-1$
+			LdapName name = new LdapName(author);
+			for(int i = name.size()-1; i >= 0; i--) {
+				String bit = name.get(i);
+				if(bit.startsWith("cn=")) { //$NON-NLS-1$
+					author = bit.substring(3);
+				}
+			}
+		}
+		entry.setAuthor(author);
+		
 		entry.setTitle(post.getTitle());
-		entry.setLink(translation.getString("baseUrl") + servletContext.getContextPath() + "/posts/" + post.getId()); //$NON-NLS-1$ //$NON-NLS-2$
+//		entry.setLink(translation.getString("baseUrl") + servletContext.getContextPath() + "/posts/" + post.getPostedYear() + "/" + post.getPostedMonth() + "/" + post.getPostedDay() + "/" + post.getSlug()); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$ //$NON-NLS-5$
+		entry.setLink(PathUtil.concat(uriInfo.getBaseUri().toString(), "posts", '/') + "/" + post.getPostedYear() + "/" + post.getPostedMonth() + "/" + post.getPostedDay() + "/" + post.getSlug()); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$ //$NON-NLS-5$
 		entry.setPublishedDate(Date.from(post.getPosted().toInstant()));
 		SyndContent content = new SyndContentImpl();
 		content.setType(MediaType.TEXT_HTML);
+		// TODO consider parsing and manipulating the HTML to have a base for images
 		content.setValue(post.getBodyHtml());
 		entry.setContents(Arrays.asList(content));
 		
