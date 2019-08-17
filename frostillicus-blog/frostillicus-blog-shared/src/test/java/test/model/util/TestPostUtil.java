55315e2dded4a0b13e53c59ef3339d95fccb65c2
Jesse Gallagher <jesse@secondfoundation.org>
2019-08-17 11:07:33 -0400

    Move test.TestPostUtil -> test.util.TestPostUtil for consistency

--- frostillicus-blog/frostillicus-blog-shared/src/test/java/test/model/util/TestPostUtil.java
+++ frostillicus-blog/frostillicus-blog-shared/src/test/java/test/model/util/TestPostUtil.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package test;
+package test.model.util;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
