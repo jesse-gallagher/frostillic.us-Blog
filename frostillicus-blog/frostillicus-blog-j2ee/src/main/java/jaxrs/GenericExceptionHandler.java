7240745c13921db47fc1fc6d3407e12914339e3c
Jesse Gallagher <jesse@secondfoundation.org>
2019-08-24 16:55:34 -0400

    Improve JAX-RS error handling (Issue #52)

--- frostillicus-blog/frostillicus-blog-j2ee/src/main/java/jaxrs/GenericExceptionHandler.java
+++ frostillicus-blog/frostillicus-blog-j2ee/src/main/java/jaxrs/GenericExceptionHandler.java
@@ -15,24 +15,56 @@
  */
 package jaxrs;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
+import javax.inject.Inject;
+import javax.mvc.Models;
+import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.NotFoundException;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
 import javax.ws.rs.ext.ExceptionMapper;
 import javax.ws.rs.ext.Provider;
 
 /**
- * Though this class doesn't do anything itself, its presence causes the web container to
- * properly direct to the configured error handler when accessing an un-mapped JAX-RS
- * resource. 
- * 
  * @author Jesse Gallagher
- *
  */
 @Provider
 public class GenericExceptionHandler implements ExceptionMapper<Throwable> {
+	@Context
+	private HttpServletRequest req;
+	
+	@Inject
+	private Models models;
 	
 	@Override
-	public Response toResponse(Throwable exception) {
-		return null;
+	public Response toResponse(Throwable t) {
+		Status status;
+		if(t instanceof NotFoundException) {
+			status = Status.NOT_FOUND;
+		} else {
+			status = Status.INTERNAL_SERVER_ERROR;
+		}
+		
+		models.put("ERROR_MESSAGE", t.getLocalizedMessage()); //$NON-NLS-1$
+		models.put("CONTEXT_PATH", req.getContextPath()); //$NON-NLS-1$
+		models.put("ERROR_STATUS_CODE", status); //$NON-NLS-1$
+        try(StringWriter w = new StringWriter()) {
+            try(PrintWriter p = new PrintWriter(w)) {
+                t.printStackTrace(p);
+            }
+            models.put("ERROR_STACK_TRACE", w.toString()); //$NON-NLS-1$
+        } catch(IOException e) {
+        		// Ignore
+        }
+		
+        // TODO customize for non-HTML requests
+		return Response.status(status)
+			.entity("error.jsp") //$NON-NLS-1$
+			.build();
 	}
 
 }
---
+++ frostillicus-blog/frostillicus-blog-j2ee/src/main/webapp/WEB-INF/views/error.jsp
@@ -0,0 +1,50 @@
+<!--
+
+    Copyright © 2012-2019 Jesse Gallagher
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+
+-->
+<!DOCTYPE html>
+<html lang="en-us">
+	<head>
+		<meta http-equiv="x-ua-compatible" content="ie=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no" />
+		
+		<link rel="shortcut icon" href="${CONTEXT_PATH}/icon.png" />
+		<link rel="apple-touch-icon" href="${CONTEXT_PATH}/icon.png" />
+		<link rel="stylesheet" href="${CONTEXT_PATH}/css/style.css" />
+		<link rel="stylesheet" href="${CONTEXT_PATH}/css/forms.css" />
+		
+		<title>frostillic.us Error</title>
+	</head>
+	<body>
+		<div id="entirety">
+			<header id="pageheader">
+			</header>
+			<nav id="pagenav">
+				
+			</nav>
+			<div id="pagebody">
+				<h2 class="title">Error</h2>
+				<p class="error" style="color: red">
+					${ERROR_MESSAGE}
+				</p>
+				<pre style="white-space: pre-wrap">${ERROR_STACK_TRACE}</pre>
+			</div>
+			<footer id="pagefooter">
+				
+			</footer>
+		</div>
+	</body>
+</html>
