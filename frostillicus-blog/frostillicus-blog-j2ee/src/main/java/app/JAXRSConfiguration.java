34cb1d64d3e7b4d4d2a9631879accb52cbc2fee1
Jesse Gallagher <jesse@secondfoundation.org>
2023-05-10 16:01:38 -0400

    Remove now-gone MVC property

--- frostillicus-blog/frostillicus-blog-j2ee/src/main/java/app/JAXRSConfiguration.java
+++ frostillicus-blog/frostillicus-blog-j2ee/src/main/java/app/JAXRSConfiguration.java
@@ -28,8 +28,7 @@ public class JAXRSConfiguration extends Application {
 	@Override
 	public Map<String, Object> getProperties() {
 		return Map.of(
-			Csrf.CSRF_PROTECTION, Csrf.CsrfOptions.EXPLICIT,
-			Properties.HIDDEN_METHOD_FILTER_ACTIVE, true
+			Csrf.CSRF_PROTECTION, Csrf.CsrfOptions.EXPLICIT
 		);
 	}
 }
